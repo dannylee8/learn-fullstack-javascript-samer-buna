00eac73fa78adc3dff5d625dd986834939214f8a
Danny Lee <dreamer@panix.com>
2020-06-04 22:13:32 -0400

    Package added: ejs

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import config from "./config";
+import apiRouter from "./api";
 
 import express from "express";
 const server = express();
 
+server.set("view engine", "ejs");
+
 server.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.render("index");
 });
 
+server.use("/api", apiRouter);
 server.use(express.static("public"));
 
 // server.get("/about.html", (req, res) => {
