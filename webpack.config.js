063ce5dd0f9f047c148ae8eb8a6470695efb4178
Danny Lee <dreamer@panix.com>
2020-06-05 20:54:17 -0400

    fix package.json to work with babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
+const path = require('path');
+
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + "/public",
-    filename: "bundle.js",
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-      },
-    ],
-  },
-};
+    entry: './src/index.js',
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+        ]
+    }
+  }
